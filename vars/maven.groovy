a7d1bd56141bbcbefdc79c43796d36cfc78dca75
Patricio Santa Cruz <pato.sta.cruz@gmail.com>
2022-01-29 01:40:32 -0300

    actualizacion

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -5,39 +5,49 @@
 */
 
 def call(){
-  
-    stage('Compile') {
-		STAGE = env.STAGE_NAME
-        bat "mvn clean compile -e"
-    }
-		
-	stage('Sonar') {
-		STAGE = env.STAGE_NAME
-        def scannerHome = tool 'sonar-scanner';
-        withSonarQubeEnv('sonar-server') {
-        bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-maven-developer -Dsonar.sources=src -Dsonar.java.binaries=build"
-			}
-	}
-          
-    stage('Test') {
-		STAGE = env.STAGE_NAME
-        bat "mvn clean test -e"
-    }
+	if (params.Stage.contains('compile')) {
+		stage('Compile') {
+			STAGE = env.STAGE_NAME
+	        bat "mvn clean compile -e"
+	    }
+	} else { println 'No ha especificado ejecutar el Stage: COMPILE' }
 	
-    stage('Package') {
-		STAGE = env.STAGE_NAME
-        bat "mvn clean package -e"
-    }
-		
-    stage('Run') {
-		STAGE = env.STAGE_NAME
-        bat "start /min mvn spring-boot:run &"
-    }
+	if(params.Stage.contains('sonar')){
+		stage('Sonar') {
+			STAGE = env.STAGE_NAME
+	        def scannerHome = tool 'sonar-scanner';
+	        withSonarQubeEnv('sonar-server') {
+	        bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-maven-developer -Dsonar.sources=src -Dsonar.java.binaries=build"
+				}
+		}
+	} else { println 'No ha especificado ejecutar el Stage: SONAR' } 
+			
+	if(params.Stage.contains('test')){
+		stage('Test') {
+			STAGE = env.STAGE_NAME
+	        bat "mvn clean test -e"
+	    }
+	} else { println 'No ha especificado ejecutar el Stage: TEST' }	
+	          
+	if(params.Stage.contains('package')){    
+	    stage('Package') {
+			STAGE = env.STAGE_NAME
+	        bat "mvn clean package -e"
+	    }
+	} else { println 'No ha especificado ejecutar el Stage: PACKAGE' }
 	
-    stage('Test Applications') {
-		STAGE = env.STAGE_NAME
-        bat "start chrome http://localhost:8081/rest/mscovid/test?msg=testing"
-	}
-}
+	if(params.Stage.contains('run')){		
+	    stage('Run') {
+			STAGE = env.STAGE_NAME
+	        bat "start /min mvn spring-boot:run &"
+	    }
+	} else { println 'No ha especificado ejecutar el Stage: RUN' }
 
+	if(params.Stage.contains('testApp')){		
+	    stage('Test Applications') {
+			STAGE = env.STAGE_NAME
+	        bat "start chrome http://localhost:8081/rest/mscovid/test?msg=testing"
+		}
+	} else { println 'No ha especificado ejecutar el Stage: Test Applications' }
+} 
 return this;
