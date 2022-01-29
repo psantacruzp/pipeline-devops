71b8a95f1cee8a9e3388d522a4539d465007d63c
Patricio Santa Cruz <pato.sta.cruz@gmail.com>
2022-01-29 02:46:31 -0300

    actualizacion de directorios

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -46,11 +46,9 @@ def call(String pipelineType){
 
 		if (params.Stage.contains('upload')) {	
 			stage('UploadNexus') {
-	            steps {
-	                script {
-	                    nexusPublisher nexusInstanceId: 'nexus-server', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'C:/Users/psantacruz/Documents/diplomado-devops/ejemplo-gradle2/ejemplo-gradle/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
-	                }
-	            }
+				STAGE = env.STAGE_NAME
+				figlet "${STAGE}"
+	          nexusPublisher nexusInstanceId: 'nexus-server', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'C:/Users/psantacruz/Documents/diplomado-devops/ejemplo-gradle2/ejemplo-gradle/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
 	        }
 	  } else { println 'No ha especificado ejecutar el Stage: UPLOAD' }
 
@@ -84,11 +82,9 @@ def call(String pipelineType){
 
 		if (params.Stage.contains('upload')) {	
 			stage('UploadNexus') {
-	            steps {
-	                script {
-	                    nexusPublisher nexusInstanceId: 'nexus-server', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'C:/Users/psantacruz/Documents/diplomado-devops/ejemplo-gradle2/ejemplo-gradle/build/DevOpsUsach2020-1.0.0.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
-	                }
-	            }
+				STAGE = env.STAGE_NAME
+				figlet "${STAGE}"
+	          nexusPublisher nexusInstanceId: 'nexus-server', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'C:/Users/psantacruz/Documents/diplomado-devops/ejemplo-gradle2/ejemplo-gradle/build/DevOpsUsach2020-1.0.0.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
 	        }
 	  } else { println 'No ha especificado ejecutar el Stage: UPLOAD' }
 	}
