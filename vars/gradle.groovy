77e95327d0da20e5781393fbf9c6b2eace672aab
Patricio Santa Cruz <pato.sta.cruz@gmail.com>
2022-01-29 10:57:27 -0300

    se modifican directorios para encontrar JAR

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -48,7 +48,7 @@ def call(String pipelineType){
 			stage('UploadNexus') {
 				STAGE = env.STAGE_NAME
 				figlet "${STAGE}"
-	          nexusPublisher nexusInstanceId: 'nexus-server', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'C:/ProgramData/Jenkins/.jenkins/workspace/_pipeline-ci_feature-dir-inicial/build/DevOpsUsach2020-0.0.2.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+	          nexusPublisher nexusInstanceId: 'nexus-server', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'C:/ProgramData/Jenkins/.jenkins/workspace/e_15_pipeline-ci_feature-library/build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
 	        }
 	  } else { println 'No ha especificado ejecutar el Stage: UPLOAD' }
 
@@ -84,7 +84,7 @@ def call(String pipelineType){
 			stage('UploadNexus') {
 				STAGE = env.STAGE_NAME
 				figlet "${STAGE}"
-	          nexusPublisher nexusInstanceId: 'nexus-server', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'C:/Users/psantacruz/Documents/diplomado-devops/ejemplo-gradle2/ejemplo-gradle/build/DevOpsUsach2020-1.0.0.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
+	          nexusPublisher nexusInstanceId: 'nexus-server', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'C:/Users/psantacruz/Documents/diplomado-devops/ejemplo-gradle2/ejemplo-gradle/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '2.0.0']]]
 	        }
 	  } else { println 'No ha especificado ejecutar el Stage: UPLOAD' }
 	}
